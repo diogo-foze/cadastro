14bc0d4b457e6f3d3f993c4dc6e20032bb91aad3
diogo-foze <diogo@foze.com.br>
2017-03-16 17:22:50 -0300

    Update head.php

--- commun/head.php
+++ commun/head.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Cadastro">
+        <meta name="author" content="Diogo Soares Ferreira">
+        
         <link href="css/bootstrap.min.css" rel="stylesheet">
 
         <meta http-equiv="X-UA-Compatible" content="IE=edge">

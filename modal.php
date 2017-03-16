b3aff9055956b15c18f96ff7eb7376c2c28348ba
Diogo Soares <diogo@foze.com.br>
2017-03-16 01:37:09 -0300

    php

--- modal.php
+++ modal.php
@@ -18,7 +18,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+                        <h4 class="modal-title" id="exampleModalLabel">Nova Messagem</h4>
                     </div>
                     <div class="modal-body">
                         <form>
@@ -27,14 +27,14 @@
                                 <input type="text" class="form-control" id="recipient-name">
                             </div>
                             <div class="form-group">
-                                <label for="message-text" class="control-label">Message:</label>
+                                <label for="message-text" class="control-label">Messagem:</label>
                                 <textarea class="form-control" id="message-text"></textarea>
                             </div>
                         </form>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Send message</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary">Enviar Messagem</button>
                     </div>
                 </div>
             </div>
